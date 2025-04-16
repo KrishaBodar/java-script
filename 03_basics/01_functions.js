0f38e15ce3f47bdfbd4f7ac7f011e60838f919f5
KrishaBodar <krishabodar07@gmail.com>
2025-04-17 00:29:06 +0530

    change in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,11 +1,11 @@
 
 function sayMyName() {
-console.log("k");
-console.log("r");
-console.log("i");
-console.log("s");
-console.log("h");
-console.log("a");
+  console.log("k");
+  console.log("r");
+  console.log("i");
+  console.log("s");
+  console.log("h");
+  console.log("a");
 } // sayMyName() // k r i s h a
 
 // sayMyName() // k r i s h a
