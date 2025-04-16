bd3b3061c1025a2f44d5854e99b7d7b5e486beb0
KrishaBodar <krishabodar07@gmail.com>
2025-04-16 22:37:15 +0530

    about functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,18 @@
+
+function sayMyName() {
+console.log("k");
+console.log("r");
+console.log("i");
+console.log("s");
+console.log("h");
+console.log("a");
+} // sayMyName() // k r i s h a
+
+// sayMyName() // k r i s h a
+
+function addTwoNumbers(num1, num2) {
+    return num1 + num2
+} // addTwoNumbers(2, 3) // 5
+
+addTwoNumbers(2, 3) // 5
+addTwoNumbers(10, 20) // 30
