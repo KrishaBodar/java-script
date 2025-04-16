fe111be8fceb61a1e4004442b2606bd980123e39
KrishaBodar <krishabodar07@gmail.com>
2025-04-16 22:27:33 +0530

    objects API and JSON in java script

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -32,7 +32,7 @@ console.log(regularUser.fullname) // { userfullname: { firstName: 'John', lastNa
 const obj1 = {1: "one", 2: "two", 3: "three"}
 const obj2 = {1: "one", 2: "two", 3: "three"}
 
-const obj3 = {obj1, obj2}
+const obj6 = {obj1, obj2}
 console.log(obj3); // { obj1: { '1': 'one', '2': 'two', '3': 'three' }, obj2: { '1': 'one', '2': 'two', '3': 'three' } }
 
 const obj4 = Object.assign({}, obj1, obj2)
@@ -69,3 +69,55 @@ console.log(tinderUser.hasOwnProperty("email")); // false
 
 
 
+
+
+const course = {
+  courseName: "JavaScript",
+  coursePrice: 199,
+  courseDuration: "3 months",
+  courseInstructor: "John Doe",
+  courseRating: 4.5
+} // object literal
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course // destructuring  
+// console.log(courseInstructor); // John Doe
+// console.log(instructor); // John Doe
+// console.log(course.courseInstructor); // John Doe
+
+
+// this is used in react.js
+/*const navbar = (props) => {
+  return (
+    <nav>
+      <h1>{props.company}</h1>
+    </nav>
+  )
+}
+const navbar = ({company}) => {
+ 
+} */
+
+
+  
+// API 
+//JSON 
+/*
+{
+  "name" : "John",
+  "age": 30,
+  "isLoggedIn": "true",
+  "score: "100",
+  "courseCount": "4",
+
+}*/
+
+
+[
+  { id: 1, name: "John", age: 30 },
+  { id: 2, name: "Jane", age: 25 },
+  { id: 3, name: "Jack", age: 35 }
+ ] // array of objects
+
+ 
