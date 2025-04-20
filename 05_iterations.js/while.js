0ff3155bf71c5afbf4b39d9e2f79e3379bd4d747
KrishaBodar <krishabodar07@gmail.com>
2025-04-20 21:14:42 +0530

    while and do while loop in java script

---
+++ 05_iterations.js/while.js
@@ -0,0 +1,43 @@
+// while loop
+
+while (condition) { 
+//     // code to be executed
+}
+
+
+let index = 0;
+while (index < 5) {
+    console.log(index);
+    index++;
+}
+// output: 0 1 2 3 4
+
+
+let myArray = ["apple", "banana", "cherry"];
+while (index < myArray.length) {
+    console.log(myArray[index]);
+    index++;
+}
+// output: apple banana cherry
+
+
+// do while loop
+
+do{
+    // code to be executed
+}while (condition);
+
+
+let score = 0;
+
+do{
+  console.log("Your score is " + score);
+  score++;
+} while (score < 5);
+// output: 
+// Your score is 0 
+// Your score is 1 
+// Your score is 2 
+// Your score is 3 
+// Your score is 4
+
