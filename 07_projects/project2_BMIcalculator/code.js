d6f462fdf13f31518e85101d91499135d3f7e689
KrishaBodar <krishabodar07@gmail.com>
2025-04-27 17:13:19 +0530

    js of project 2

---
+++ 07_projects/project2_BMIcalculator/code.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector('#Height').value)
+    const weight = parseInt(document.querySelector('#Weight').value)
+    const results = document.querySelector('#results')
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = `Please give a valid height ${height}`;
+    }
+
+    else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = `Please give a valid height ${weight}`;
+    } 
+    
+    else {
+        const bmi = (weight / ((height*height)/1000)).toFixed(2);
+        // shoe the results
+        results.innerHTML = `<span>${bmi}</span>`;
+    }
+    
+})
