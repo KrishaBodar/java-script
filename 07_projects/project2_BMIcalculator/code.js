63e89b6a306f301444da30bc372cb0c1e9a82090
KrishaBodar <krishabodar07@gmail.com>
2025-04-27 18:20:07 +0530

    project 3 completed

--- 07_projects/project2_BMIcalculator/code.js
+++ 07_projects/project2_BMIcalculator/code.js
@@ -3,8 +3,8 @@ const form = document.querySelector('form')
 form.addEventListener('submit', function(e){
     e.preventDefault()
 
-    const height = parseInt(document.querySelector('#Height').value)
-    const weight = parseInt(document.querySelector('#Weight').value)
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
     const results = document.querySelector('#results')
 
     if (height === '' || height < 0 || isNaN(height)) {
