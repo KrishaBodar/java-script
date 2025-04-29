5550d6eebbd258ed2517ca8471eb7d17a4f3fa13
KrishaBodar <krishabodar07@gmail.com>
2025-04-29 22:08:03 +0530

    project for js p4

---
+++ 07_projects/project4_GuessNumber/code.js
@@ -0,0 +1,17 @@
+const randomNumber = parseInt(Math.random()*100+1);
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi')
+const startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p');
+
+let prevGuess = [];
+let nymGuess = 1;
+ 
+let playGame = true;
+
+
